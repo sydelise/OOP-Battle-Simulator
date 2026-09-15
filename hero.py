40a6ea876e5d2681d1e5277ac478161cb4adcc58
sydelise <sydelise2029@gmail.com>
2026-09-15 09:00:05 -0500

    adding battle

--- hero.py
+++ hero.py
@@ -1,19 +1,18 @@
+import random
 class Hero:
-  def _init_(self,name):
-   self.name=("Superman")
-   self.health=130
-   self.attack_power=50
+   def _init_(self,name):
+    self.name= name
+    self.health=130
+    self.attack_power=50
 
-def attack(self):
-    return "50 damage dealt to goblin."
-    return random.randint(1, self.attack_power)
+    def attack(self):
+     random.randint(1, self.attack_power)
 
-def take_damage(self, damage):
-  self.health= (self.health- damage)
-print(f"{self.name} takes {self.damage} damage. Health: {self.health}")
+    def take_damage(self, damage):
+        self.health= self.health- damage
+        if self.health<0:
+            self.health=0
+        print(f"{self.name} takes {damage} damage. Health: {self.health}")
 
-def is_alive(self):
-   if self.health> 120():
-     print(f"{self.name} has {self.health} health left.")
-     return self.health > 0
-
+    def is_alive(self):
+       return self.health > 0
