70ccee374bff7259afa52aa2602e79680a0fa837
sydelise <sydelise2029@gmail.com>
2026-09-15 20:25:13 -0500

    new and improved hero class

--- game.py
+++ game.py
@@ -4,17 +4,17 @@ from hero import Hero
 ARENA_NAME = "The Iron Square"
 
 def battle(hero:Hero,enemy:Goblin):
-    while hero.is_alive and enemy.is_alive():
+    while hero.is_alive() and enemy.is_alive():
         hero_damage= hero.attack()
         enemy.take_damage(hero_damage)
         if enemy.is_alive():
-              enemy_damge=enemy.attack
-              hero.take_damage(enemy_damge)
+              enemy_damage=enemy.attack()
+              hero.take_damage(enemy_damage)
 
               if hero.is_alive():
-                print(f"(hero.name)wins!")
+                print(f"{hero.name}wins!")
         else:
-              print(f"(enemy.name)wins!") 
+              print(f"{enemy.name}wins!") 
 
 
 
@@ -31,7 +31,7 @@ def main():
 
 
     hero=Hero("Erenyager")
-    print(f"{hero.name} enters the arena with{hero.health} health>")
+    print(f"{hero.name} enters the arena with {hero.health} health")
     battle(hero,goblin)
 
 if __name__ == "__main__":
